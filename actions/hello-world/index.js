7a755393898e4c652e9b6135885a569ae4652ff5
Carlos León <mongrelion@gmail.com>
2023-01-31 17:29:34 +0100

    Test Actions error handling capabilities

--- actions/hello-world/index.js
+++ actions/hello-world/index.js
@@ -1,1 +1,11 @@
-console.log('hello, world!');
+/**
+ * We want to test that if an error is unhandled, the Actions framework knows
+ * how to handle it
+ */
+const { cp } = require('node:fs/promises');
+
+async function main() {
+  await cp('foo', 'bar', {});
+}
+
+main();
