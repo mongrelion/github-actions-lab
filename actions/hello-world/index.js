0fae9e0b265484e1ccb1edb578d831354a957720
Carlos León <mongrelion@gmail.com>
2023-02-07 16:31:40 +0100

    Add warning

--- actions/hello-world/index.js
+++ actions/hello-world/index.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
 
 const err = new Error('This is an error object');
-core.error('This is an error message')
+core.warning('Something went wrong');
 core.error(err);
