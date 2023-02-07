d03be02abd0267652e42517e542a60d62e16f8a7
Carlos León <mongrelion@gmail.com>
2023-02-07 16:29:06 +0100

    Call only core.error

--- actions/hello-world/index.js
+++ actions/hello-world/index.js
@@ -3,5 +3,3 @@ const core = require('@actions/core');
 const err = new Error('This is an error object');
 core.error('This is an error message')
 core.error(err);
-core.setFailed('Something went wrong');
-throw new Error('Something went terribly wrong');
