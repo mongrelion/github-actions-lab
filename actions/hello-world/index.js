f06f8884c6ed763ab0e6d99c8ae13a2254f4457e
Carlos León <mongrelion@gmail.com>
2023-02-07 16:24:54 +0100

    Invoke core.error

--- actions/hello-world/index.js
+++ actions/hello-world/index.js
@@ -1,4 +1,7 @@
 const core = require('@actions/core');
 
+const err = new Error('This is an error object');
+core.error('This is an error message')
+core.error(err);
 core.setFailed('Something went wrong');
 throw new Error('Something went terribly wrong');
